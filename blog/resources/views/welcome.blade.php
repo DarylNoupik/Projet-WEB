1f50c678c56a0472887ab40a369549d2358d0872
DarylNoupik <daryl.kuitche.cm@viacesi.fr>
2023-04-26 02:15:55 +0100

    ajustement de la page d'acceuil

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -21,6 +21,104 @@
         </div> 
     </div>
 </div>
+
+<section class="hero-banner untree_co-section bg-light">
+    <div class="container">
+    <div class="row align-items-center">
+    <div class="col-lg-5">
+    <div class="hero-banner__img">
+    <img class="img-fluid" src="images/hero-banner.png" alt="">
+    </div>
+    </div>
+    <div class="col-lg-7 pl-lg-5">
+    <div class="hero-banner__content">
+    <h1>Bienvenue sur BDE.<span class="text-primary">Com</span></h1>
+    <p>Le bureau des étudiants vous accompagne tout au long de l'année dans l'organisation d'évènements et la mise en place d'activités étudiantes</p>
+    <a class="btn btn-primary btn-rounded" href="#">Découvrir nos activités</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
+    <section class="untree_co-section bg-light">
+        <div class="container">
+        <div class="row">
+        <div class="col-md-4 p-0">
+        <div class="feature-item set-bg" data-setbg="images/feature-1.jpg">
+        <div class="fi-content text-white">
+        <h5><a href="#">Manifestations</a></h5>
+        <p>Retrouvez toutes nos manifestations passées et à venir, et inscrivez-vous pour y participer</p>
+        <a href="#" class="site-btn">Voir les évènements</a>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 p-0">
+        <div class="feature-item set-bg" data-setbg="images/feature-2.jpg">
+        <div class="fi-content text-white">
+        <h5><a href="#">Boutique en ligne</a></h5>
+        <p>Retrouvez notre boutique en ligne avec tous les goodies et produits dérivés BDE</p>
+        <a href="#" class="site-btn">Visiter la boutique</a>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 p-0">
+        <div class="feature-item set-bg" data-setbg="images/feature-3.jpg">
+        <div class="fi-content text-white">
+        <h5><a href="#">Contactez-nous</a></h5>
+        <p>Vous avez des questions ou des suggestions ? Contactez-nous directement depuis notre page dédiée</p>
+        <a href="#" class="site-btn">Nous contacter</a>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </section>
+        <section class="untree_co-section bg-light">
+        <div class="container">
+        <div class="section-title text-center">
+        <h2>Activités populaires</h2>
+        </div>
+        <div class="row">
+        <div class="col-lg-4 col-md-6">
+        <div class="single-popular-item">
+        <img src="images/popular-1.jpg" alt="">
+        <h5>Soirée étudiante</h5>
+        <a href="#" class="primary-btn">S'inscrire</a>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+        <div class="single-popular-item">
+        <img src="images/popular-2.jpg" alt="">
+        <h5>Concert de musique</h5>
+        <a href="#" class="primary-btn">S'inscrire</a>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+        <div class="single-popular-item">
+        <img src="images/popular-3.jpg" alt="">
+        <h5>Tournoi de jeux-vidéo</h5>
+        <a href="#" class="primary-btn">S'inscrire </a>
+    </div>
+</div>
+</div>
+</div>
+</section>
+<section class="untree_co-section bg-light">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="newsletter">
+<h2>Abonnez-vous à notre newsletter</h2>
+<form>
+<input type="email" placeholder="Entrez votre adresse e-mail">
+<button class="site-btn">S'abonner</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</section>
+
 <!-- Section About -->
 <div class="untree_co-section bg-light">
 <div class="container">
